336c83c918ead1c61281e45d59cd9fc6de8a215f
Tuna Dökmeci <tuna.dokmeci@sciencespo.fr>
2019-12-05 16:12:54 +0100

    data preparation for event history analysis

--- Mincome/Codes/dataprep.R
+++ Mincome/Codes/dataprep.R
@@ -94,13 +94,6 @@ basepaypanel <- basepaypanel %>%
   arrange(FAMNUM, month)
 
 
-#we have 128 increases in the number of children overall 
-
-#for some families, family size, ages of householders are missing because they were not 
-#in the baseline survey
-#we take the values for these variables in the first month they were enrolled 
-#and merge it back to the panel data
-
 faminfo <- subset(basepaypanel, select=c("FAMNUM","FSI", "FS", "month1", "fage", "mage"))
 faminfo <- faminfo[which(faminfo$month1 ==1),]
 faminfo <- faminfo %>% rename(FAMSI=FSI)
@@ -109,8 +102,6 @@ faminfo <- faminfo %>% rename(FAGE=fage)
 faminfo <- faminfo %>% rename(MAGE=mage)
 
 faminfo <- faminfo[,-4]
-
-#we need to transform it back to cross section 
 #have a dummy that indicates if there has been any increases
 
 basepaypanel$sum = 0
@@ -118,7 +109,7 @@ basepaypanel$sum = 0
 basepaypanel$FAMNUM <- as.factor(basepaypanel$FAMNUM)
 for (i in levels(basepaypanel$FAMNUM)){
   s <- sum(basepaypanel[basepaypanel$FAMNUM == i, "increase"])
-basepaypanel$sum[basepaypanel$FAMNUM == i] <- s
+  basepaypanel$sum[basepaypanel$FAMNUM == i] <- s
 }
 
 basepaypanel$if_increase = 0   
@@ -168,155 +159,116 @@ basepaypanel$if_change[basepaypanel$summ != 0] <- 1
 
 basepaypanel<-merge(basepaypanel, faminfo, by = "FAMNUM", all=T)
 
+basepaypanel <- basepaypanel %>%
+  group_by(FAMNUM) %>%
+  arrange(month)  %>%
+  mutate(prevtreat = dplyr::lag(plan, 1))  %>%
+  ungroup()%>%
+  arrange(FAMNUM, month)
+
+basepaypanel$changetreatment[basepaypanel$prevtreat !=basepaypanel$plan] = 1 
+
 basepaypanel_rem <- basepaypanel[which(basepaypanel$AC != 0),]
 
 basepaypanel_rem <- basepaypanel_rem[which(basepaypanel_rem$if_change == 0),]
 
-#do everything numeric so to get their means 
-basepaypanel$plan = as.numeric(as.character(basepaypanel$plan))
-basepaypanel$Winni <- as.numeric(as.character(basepaypanel$Winni))
-basepaypanel$Dauphin <- as.numeric(as.character(basepaypanel$Dauphin))
-basepaypanel$DH <- as.numeric(as.character(basepaypanel$DH))
-basepaypanel$SH <- as.numeric(as.character(basepaypanel$SH))
-basepaypanel$mage <- as.numeric(as.character(basepaypanel$FAGE))
-basepaypanel$fage <- as.numeric(as.character(basepaypanel$MAGE))
-basepaypanel$TOTNHINC73 <- as.numeric(as.character(basepaypanel$TOTNHINC73))
-basepaypanel$TOTNHINC74 <- as.numeric(as.character(basepaypanel$TOTNHINC74))
-basepaypanel$W <- as.numeric(as.character(basepaypanel$W))
-basepaypanel$TRN <- as.numeric(as.character(basepaypanel$TRN))
-basepaypanel$FAMSI  <- as.numeric(as.character(basepaypanel$FAMSI))
-basepaypanel$FAMS <- as.numeric(as.character(basepaypanel$FAMS))  
+basepaypanel_winni <- basepaypanel_rem[which(basepaypanel_rem$SITECODE == 1),]
 
-basepaypanel_rem$plan = as.numeric(as.character(basepaypanel_rem$plan))
-basepaypanel_rem$Winni <- as.numeric(as.character(basepaypanel_rem$Winni))
-basepaypanel_rem$Dauphin <- as.numeric(as.character(basepaypanel_rem$Dauphin))
-basepaypanel_rem$DH <- as.numeric(as.character(basepaypanel_rem$DH))
-basepaypanel_rem$SH <- as.numeric(as.character(basepaypanel_rem$SH))
-basepaypanel_rem$FAGE <- as.numeric(as.character(basepaypanel_rem$FAGE))
-basepaypanel_rem$MAGE <- as.numeric(as.character(basepaypanel_rem$MAGE))
-basepaypanel_rem$TOTNHINC73 <- as.numeric(as.character(basepaypanel_rem$TOTNHINC73))
-basepaypanel_rem$TOTNHINC74 <- as.numeric(as.character(basepaypanel_rem$TOTNHINC74))
-basepaypanel_rem$W <- as.numeric(as.character(basepaypanel_rem$W))
-basepaypanel_rem$TRN <- as.numeric(as.character(basepaypanel_rem$TRN))
-basepaypanel_rem$FAMS  <- as.numeric(as.character(basepaypanel_rem$FAMS))
-basepaypanel_rem$FAMSI <- as.numeric(as.character(basepaypanel_rem$FAMSI))  
-
-
-basepaycross_rem <- basepaypanel_rem %>%
-  group_by(FAMNUM)%>%
-  select(FAMNUM, if_increase, plan, Winni,Dauphin,  DH, SH, individual, control,
-         FAGE, MAGE, TOTNHINC73, TOTNHINC74, FAMS, FAMSI, W, TRN) %>%
-  summarise(if_increase=mean(if_increase), 
-            plan = mean(plan), Winni = mean(Winni), 
-            Dauphin = mean(Dauphin), DH = mean(DH),
-            SH = mean(SH), individual = mean(individual), control = mean(control),
-            FAGE = mean(FAGE), MAGE = mean(MAGE), TOTNHINC73 = mean(TOTNHINC73),
-            TOTNHINC74 = mean(TOTNHINC74), 
-            meanwage = mean(W), meaninc = mean(TRN),
-              FAMS = mean(FAMS), FAMSI = mean(FAMSI))
-
-basepaycross_rem$plan <- as.factor(basepaycross_rem$plan)
-basepaycross_rem$control <- as.factor(basepaycross_rem$control)
-basepaycross_rem$if_increase <- as.factor(basepaycross_rem$if_increase)
-basepaycross_rem$Winni <- as.factor(basepaycross_rem$Winni)
-basepaycross_rem$Dauphin <- as.factor(basepaycross_rem$Dauphin)
-basepaycross_rem$DH <- as.factor(basepaycross_rem$DH)
-basepaycross_rem$SH <- as.factor(basepaycross_rem$SH)
-basepaycross_rem$individual <- as.factor(basepaycross_rem$individual)
-
-
-
-basepaypanel_winni <- basepaypanel[which(basepaypanel$SITECODE == 1),]
+basepaypanel_winni$year <- 0 
+basepaypanel_winni$year[basepaypanel_winni$month < 12] <- 1975
+basepaypanel_winni$year[basepaypanel_winni$month >= 12 & basepaypanel_winni$month < 24] <- 1976
+basepaypanel_winni$year[basepaypanel_winni$month >= 24 & basepaypanel_winni$month <= 37] <- 1977
 saveRDS(basepaypanel_winni, "basepaypanel_winni.rds")
 
-#family data to calculate the age where women have kids 
-
+#organising the data in women´s fertility histories with each row corresponding to a year between ages 15 and 50 
 famdata_ind <- read_dta("W:/WU/Projekte/mincome/Mincome/Data/famdata_ind.dta")
 famdata_ind[famdata_ind == -4] <- NA
 famdata_ind <- famdata_ind[, c(1:6)]
 famdata_ind <- famdata_ind[!(is.na(famdata_ind$OID)), ]
-famdata_ind <- famdata_ind[famdata_ind$RTH == 2 | famdata_ind$RTH == 4 | famdata_ind$RTH == 5, ]
+famdata_ind <- famdata_ind[famdata_ind$
+                             RTH == 2 | famdata_ind$RTH == 4 | famdata_ind$RTH == 5, ]
 
 famdata_ind$BIRTH <- as.character(famdata_ind$BIRTH)
 famdata_ind$birthyear <- substr(famdata_ind$BIRTH, 1, 2)
 famdata_ind$birthday <- substr(famdata_ind$BIRTH, 3, 6)
+famdata_ind$birthyear <- paste("19", famdata_ind$birthyear, sep="")
+
 
 famdata_ind$birthyear <- as.numeric(famdata_ind$birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_firstch = dplyr::lag(birthyear, 1) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_firstch = dplyr::lag(birthyear, 1) - birthyear)  %>%
   ungroup()%>%
   arrange(FAMNUM, BIRTH)
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_secch = dplyr::lag(birthyear, 2) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_thirdch = dplyr::lag(birthyear, 3) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_fourthch = dplyr::lag(birthyear, 4) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_fifthch = dplyr::lag(birthyear, 5) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_sixthch = dplyr::lag(birthyear, 6) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_seventhch = dplyr::lag(birthyear, 7) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_eigthch = dplyr::lag(birthyear, 8) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
-
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_ninthch = dplyr::lag(birthyear, 9) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind%>%
   group_by(FAMNUM) %>%
-  arrange(desc(BIRTH))  %>%
+  arrange(desc(birthyear))  %>%
   mutate(age_tenthch = dplyr::lag(birthyear, 10) - birthyear)  %>%
   ungroup()%>%
-  arrange(FAMNUM, BIRTH)
+  arrange(FAMNUM, birthyear)
 
 famdata_ind <- famdata_ind[famdata_ind$RTH == 2, ]
 
@@ -348,10 +300,56 @@ famdata_ind$event[famdata_ind$age_eigthch == famdata_ind$age] <- 1
 famdata_ind$event[famdata_ind$age_ninthch == famdata_ind$age] <- 1
 famdata_ind$event[famdata_ind$age_tenthch == famdata_ind$age] <- 1
 
+#here we have the births that happened before the experiment start as well 
+#as those that were recorded. we need to manually add the increases in the number of children 
+#that we computed using baseline 
+#payments data but that were absent from the family information data 
+
+#information on basepay panel is monthly and we want to see if there has been an increase in that given year 
+
+basepaypanel_winni$FAMNUM <- as.factor(basepaypanel_winni$FAMNUM)
+basepaypanel_winni$year <- as.factor(basepaypanel_winni$year)
+basepaypanel_winni$increase <- as.numeric(basepaypanel_winni$increase)
+
+for (i in levels(basepaypanel_winni$FAMNUM)){
+    for (j in levels(basepaypanel_winni$year)){
+  s <- sum(basepaypanel_winni[basepaypanel_winni$FAMNUM == i & basepaypanel_winni$year == j, "increase"])
+  basepaypanel_winni$summm[basepaypanel_winni$FAMNUM == i & basepaypanel_winni$year == j] <- s
+    }
+}
+
+basepaypanel_winni$if_birth = 0   
+basepaypanel_winni$if_birth[basepaypanel_winni$summm != 0] <- 1
+
+basepaypanel_winni$plan <- as.numeric(as.character(basepaypanel_winni$plan))
+basepaypanel_winni$control <- as.numeric(as.character(basepaypanel_winni$control))
+
+basepaypanel_winni_year <- basepaypanel_winni %>%
+  group_by(FAMNUM, year)%>%
+  select(FAMNUM, if_birth, year, control, plan) %>%
+  summarise(if_birth=mean(if_birth), control = mean(control), plan =mean(plan))
+
+famdata_ind <- merge(basepaypanel_winni_year, famdata_ind, by =c("FAMNUM", "year"), all=T)
 famdata_ind$year = famdata_ind$birthyear + famdata_ind$age
-famdata_ind$year <- paste("19", famdata_ind$year, sep="")
+famdata_ind$event[famdata_ind$if_birth == 1] <- 1
+famdata_ind <- famdata_ind%>%
+  arrange(OID, age) 
+#for all women who reach the age 50 by the end of experiment, we have the full birth history
+#for women who are under 50 by the end of experiment, the years after the experiment are censored 
 
+famdata_ind$censored <-0
+famdata_ind$censored[famdata_ind$year > 1977] <- 1
+length(which(famdata_ind$event == 1))
 
+#have the control variable be 1 or 0 for all years 
+famdata_ind$OID <- as.factor(as.character(famdata_ind$OID))
+for (i in levels(famdata_ind$OID)){
+  s <- sum(famdata_ind[famdata_ind$OID == i, "control"])
+  famdata_ind$sumcontrol[famdata_ind$FAMNUM == i] <- s
+}
+
+famdata_ind$control1 = 0   
+famdata_ind$control1[basepaypanel_winni$sumcontrol != 0] <- 1
 
 length(which(famdata_ind$event == 1 & famdata_ind$age == 15))
 length(which(famdata_ind$event == 1 & famdata_ind$age == 16))
@@ -389,18 +387,4 @@ length(which(famdata_ind$event == 1 & famdata_ind$age == 47))
 length(which(famdata_ind$event == 1 & famdata_ind$age == 48))
 length(which(famdata_ind$event == 1 & famdata_ind$age == 49))
 length(which(famdata_ind$event == 1 & famdata_ind$age == 50))
-
-#here we have the births that happened before the experiment start as well 
-#as those that were recorded. we need to manually add the increases in the number of children 
-#that we computed using baseline payments data but that were absent from the family information data 
-
-basepaypanel_winni$year <- 0 
-basepaypanel_winni$year[basepaypanel_winni$month < 12] <- 1975
-basepaypanel_winni$year[basepaypanel_winni$month >= 12 & basepaypanel_winni$month < 24] <- 1976
-basepaypanel_winni$year[basepaypanel_winni$month >= 24 & basepaypanel_winni$month <= 37] <- 1977
-
-
-saveRDS(famdata_ind, file = "famdata_ind.rds")
-
-
 
