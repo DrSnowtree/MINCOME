68b8ff96e291fd471a463b1adfa24aed742aba50
Tuna Dökmeci <tuna.dokmeci@sciencespo.fr>
2020-01-07 14:13:33 +0100

    new code for descriptives participation

--- Mincome/Codes/participation_desc.R
+++ Mincome/Codes/participation_desc.R
@@ -1,9 +1,4 @@
-#code to compare the households who dropped out within the first two years 
-#and those that continued
-
-library("compareGroups")
-library("data.table")
-library("gtools")
+library(readxl)
 library("haven")
 library("dplyr")
 library("tidyr") 
@@ -11,26 +6,17 @@ library("tidyverse")
 library("lubridate") 
 library("data.table")
 library("foreign")
-library("quantmod") 
-library("zoo")
-library("plm")
+library("haven")
 library("gplots")
 library("stargazer")
-library("lfe")
 library("Hmisc")
-library("readxl")
-library("naniar")
-library("strex")
-library(devtools)
 
-#download data
-base_pay <- read_excel("Downloads/base_pay.data_revised_Dec 11, 2019.xlsx")
-View(base_pay)
-baseline <- read_excel("Downloads/baseline.data_labeled.xlsx")
 
-#households that are in baseline data but not in baseline_pay are the ones for whom we don't have 
-#payment info so they dropped out within the first two years 
 
+base_payrev <- read_excel("W:/WU/Projekte/mincome/Mincome/Data/base_pay.data_revised_Dec 11, 2019.xlsx")
+View(base_payrev)
+baseline <- read_excel("W:/WU/Projekte/mincome/Mincome/Data/baseline.xlsx")
+View(baseline)
 stata.merge <- function(x,y, by = intersect(names(x), names(y))){
   
   x[is.na(x)] <- Inf
@@ -52,97 +38,93 @@ stata.merge <- function(x,y, by = intersect(names(x), names(y))){
 }
 
 
-base_pay %>% 
-  rename(
-    FAMNUM = `Fam Num`,
-    HHEAD = `Double Head = 1...94`, 
-    SHEAD = `Single Head = 1...95`
-    AGEM = `Age Male Head...96`
-    AGEF = `Age Female Head...97`
-    FAMSIZE = FSI37
-    NUMADULTS = 
-    
-  )
 
-mergedbasepay <- stata.merge(base_pay,baseline, by = c("FAMNUM", "HHEAD", "SHEAD",
-                                                       "AGEM", "AGEF", "FAMSIZE", "NUMADULTS", "NUMCHILD"
-                                                       , "FAMSIZEI", "HMEOWNER", "TREAT", "TOTNHINC74",
-                                                       "TOTNHINC73", "TOTFAMINC74", "MHGROSSEARN", "FHGROSSEARN"))
+base_payrev$FAMNUM <- base_payrev$`Fam Num`
 
 
-#matched are the ones that have continued for at least two years, using the ones that dropped out within 
-#two years, master the ones that joined after (probably supplemental sample)
+nonpart <- stata.merge(base_payrev, baseline, by = "FAMNUM")
 
-#we can compare the matched and the using to see if they are fundamentally different
+nonpart <- nonpart %>% 
+  filter(`Site Code` == 1|`SITE CODE` == 1) 
 
-mergedbasepay1 <- dplyr::filter(mergedbasepay, merge == "matched" | merge == "using") 
-mergedbasepay1$enrolled <- 0
-mergedbasepay1$enrolled[mergedbasepay1$merge == "matched"] <- 1
-mergedbasepay1[mergedbasepay1 == -9] <- NA
-mergedbasepay1[mergedbasepay1 == -7] <- NA
 
-mergedbasepay1 <- mergedbasepay1[-c(2174, 2175, 2176, 2177),] 
-mergedbasepay1$individual <- 0 
-mergedbasepay1$individual[mergedbasepay1$HHEAD == 0 & mergedbasepay1$SHEAD == 0] <- 1
+nonpart$part <- 0 
+nonpart$part[nonpart$merge == "matched"] <- 1 
+nonpart$AGEF <- as.numeric(nonpart$AGEF)
+nonpart[nonpart == -9] <- NA
+nonpart[nonpart == -7] <- NA
+nonpart[nonpart == -1] <- NA
+nonpart[nonpart == "."] <- NA
+nonpart$individual <- 0 
+nonpart$individual[nonpart$HHEAD== 0 & nonpart$SHEAD == 0] <- 1
 
-mergedbasepay1$MHNOTLOOK.y <- as.factor(mergedbasepay1$MHNOTLOOK.y)
-mergedbasepay1$MHLABPART.y <- as.factor(mergedbasepay1$MHLABPART.y)
-mergedbasepay1$MHJOBSAT.y <- as.factor(mergedbasepay1$MHJOBSAT.y)
-mergedbasepay1$MH_SCH.y <- as.factor(mergedbasepay1$MH_SCH.y)
-mergedbasepay1$MHJOBS74.y <- as.factor(mergedbasepay1$MHJOBS74.y)
 
-mergedbasepay1$FHNOTLOOK.y <- as.factor(mergedbasepay1$FHNOTLOOK.y)
-mergedbasepay1$FHLABPART.y <- as.factor(mergedbasepay1$FHLABPART.y)
-mergedbasepay1$FHJOBSAT.y <- as.factor(mergedbasepay1$FHJOBSAT.y)
-mergedbasepay1$FH_SCH.y <- as.factor(mergedbasepay1$FH_SCH.y)
-mergedbasepay1$AGEF <- as.numeric(mergedbasepay1$AGEF)
-mergedbasepay1$AGEM <- as.numeric(mergedbasepay1$AGEM)
+nonpart$HHEAD <- as.factor(nonpart$HHEAD)
+nonpart$SHEAD <- as.factor(nonpart$SHEAD)
+nonpart$individual <- as.character(nonpart$individual)
+nonpart$TOTFAMINC74 <- as.numeric(nonpart$TOTFAMINC74)
+nonpart$MHYRSSCHL <- as.numeric(nonpart$MHYRSSCHL)
+nonpart$FHYRSSCHL <- as.numeric(nonpart$FHYRSSCHL)
+nonpart$MH_SCH <- as.factor(nonpart$MH_SCH)
+nonpart$FH_SCH <- as.factor(nonpart$FH_SCH)
+nonpart$MHLABPART <- as.factor(nonpart$MHLABPART)
+nonpart$MHJOBSAT <- as.factor(nonpart$MHJOBSAT)
+nonpart$FHLABPART <- as.factor(nonpart$FHLABPART)
+nonpart$FHJOBSAT <- as.factor(nonpart$FHJOBSAT)
+nonpart$MHCHDCARE <- as.numeric(nonpart$MHCHDCARE)
+nonpart$FHCHDCARE <- as.numeric(nonpart$FHCHDCARE)
+nonpart$NUMCHILD <- as.factor(nonpart$NUMCHILD)
+nonpart$FHTOTERN74 <- as.numeric(nonpart$FHTOTERN74)
+nonpart$FHTOTERN73 <- as.numeric(nonpart$FHTOTERN73)
+nonpart$MHTOTERN74 <- as.numeric(nonpart$MHTOTERN74)
+nonpart$MHTOTERN73 <- as.numeric(nonpart$MHTOTERN73)
+nonpart$NUMADULTS <- as.factor(nonpart$NUMADULTS)
+nonpart$TREAT <- as.factor(nonpart$TREAT)
 
-mergedbasepay1$FHTOTERN73.y <- as.numeric(mergedbasepay1$FHTOTERN73.y)
-mergedbasepay1$FHTOTERN74.y <- as.numeric(mergedbasepay1$FHTOTERN74.y)
-mergedbasepay1$FHHOURS.y <- as.numeric(mergedbasepay1$FHHOURS.y)
-mergedbasepay1$FHYRSSCHL.y <- as.numeric(mergedbasepay1$FHYRSSCHL.y)
-mergedbasepay1$FHCHDCARE.y <- as.numeric(mergedbasepay1$FHCHDCARE.y)
-mergedbasepay1$FHWEEK74.y <- as.numeric(mergedbasepay1$FHWEEK74.y) 
-mergedbasepay1$FHWEEKS73.y <- as.numeric(mergedbasepay1$FHWEEKS73.y) 
-mergedbasepay1$FAMSIZEI <- as.numeric(mergedbasepay1$FAMSIZEI) 
-mergedbasepay1$FAMSIZE <- as.numeric(mergedbasepay1$FAMSIZE) 
-mergedbasepay1$TOTFAMINC74 <- as.numeric(mergedbasepay1$TOTFAMINC74)
+label(nonpart$HHEAD) <- "Households with double householders"
+label(nonpart$SHEAD) <- "Households with single householders"
+label(nonpart$individual) <- "Single individual"
+label(nonpart$NUMADULTS) <- "Number of adults in the household"
+label(nonpart$NUMCHILD) <- "Number of children in the household"
+label(nonpart$part) <- "Remained in the experiment"
+label(nonpart$MHLABPART) <- "Labor participation in the past year (male householder)"
+label(nonpart$MHJOBSAT) <- "Job Satisfaction Index (male householder)"
+label(nonpart$MHCHDCARE) <- "Expected or actual childcare cost (male householder)"
+label(nonpart$MH_SCH) <- "Completed high school (male householder)"
+label(nonpart$MHYRSSCHL) <- "Years of schooling (male householder)"
+label(nonpart$FHLABPART) <- "Labor participation in the past year (female householder)"
+label(nonpart$FHTOTERN74) <- "Total Earnings in 1974 (female householder)"
+label(nonpart$FHTOTERN73) <- "Total Earnings in 1973 (female householder)"
+label(nonpart$MHTOTERN74) <- "Total Earnings in 1974 (male householder)"
+label(nonpart$MHTOTERN73) <- "Total Earnings in 1973 (male householder)"
+label(nonpart$FHJOBSAT) <- "Job Satisfaction Index (female householder)"
+label(nonpart$FHCHDCARE) <- "Expected or actual childcare cost (female householder)"
+label(nonpart$FH_SCH) <- "Completed high school (female householder)"
+label(nonpart$FHYRSSCHL) <- "Years of schooling (female householder)"
+label(nonpart$TOTFAMINC74) <- "Total family income in 1974"
+label(nonpart$AGEM) <- "Age of the male householder"
+label(nonpart$AGEF) <- "Age of the female householder"
+label(nonpart$TREAT) <- "Treatment plan"
+label(nonpart$TOTNHINC73) <- "Total non-householder earned income 1973"
+label(nonpart$TOTNHINC74) <- "Total non-householder earned income 1974"
 
-label(mergedbasepay1$HHEAD) <- "Double-Headed Household"
-label(mergedbasepay1$SHEAD) <- "Single-Headed Household"
-label(mergedbasepay1$individual) <- "Single Individual"
-label(mergedbasepay1$NUMADULTS) <- "Number of Adults in the Household"
-label(mergedbasepay1$NUMCHILD) <- "Number of Children in the Household"
-label(mergedbasepay1$HMEOWNER) <- "Homeowner Dummy"
-label(mergedbasepay1$TREAT) <- "Assigned Treatment Group"
-label(mergedbasepay1$enrolled) <- "Enrolled and Continued"
-label(mergedbasepay1$LIQASSETS.y) <- "Liquid Assets"
-label(mergedbasepay1$DEBTS.y) <- "Debt"
-label(mergedbasepay1$MHLABPART.y) <- "Labor Participation (Male Head)"
-label(mergedbasepay1$MHNOTLOOK.y) <- "Reason why male head did not look for work"
-label(mergedbasepay1$MHHOURS.y) <- "Number of Hours Worked Past Week (Male Head)"
-label(mergedbasepay1$MHTOTERN74.y) <- "Total Earnings in 1974 (Male Head)"
-label(mergedbasepay1$MHTOTERN73.y) <- "Rotal Earnings in 1973 (Male Head)"
-label(mergedbasepay1$MHJOBSAT.y) <- "Job Satisfaction Index (Male Head)"
-label(mergedbasepay1$MHCHDCARE.y) <- "Expected or actual childcare cost (Male head)"
-label(mergedbasepay1$MH_SCH.y) <- "Completed high school (Male head)"
-label(mergedbasepay1$FHLABPART.y) <- "Labor Participation (Female Head)"
-label(mergedbasepay1$FHNOTLOOK.y) <- "Reason why female head did not look for work"
-label(mergedbasepay1$FHHOURS.y) <- "Number of Hours Worked Past Week (Female Head)"
-label(mergedbasepay1$FHTOTERN74.y) <- "Total Earnings in 1974 (Female Head)"
-label(mergedbasepay1$FHTOTERN73.y) <- "Total Earnings in 1973 (Female Head)"
-label(mergedbasepay1$FHJOBSAT.y) <- "Job Satisfaction Index (Female Head)"
-label(mergedbasepay1$FHCHDCARE.y) <- "Expected or actual childcare cost (Female head)"
-label(mergedbasepay1$FH_SCH.y) <- "Completed high school (Female head)"
-label(mergedbasepay1$FHYRSSCHL.y) <- "Years of schooling (Female head)"
-label(mergedbasepay1$FAMSIZE) <- "Family Size"
-label(mergedbasepay1$FAMSIZEI) <- "Family Size Index"
-label(mergedbasepay1$TOTFAMINC74) <- "Total Family Income in 1974"
 
-compare = compareGroups::compareGroups(enrolled ~ .,
-                                       data=mergedbasepay1,  method = NA)
 
-compare_table <-  compareGroups::createTable(compare)
-print(compare_table)
-export2latex(createTable(compare))
+compare = compareGroups::compareGroups(part ~ AGEF + AGEM + NUMADULTS 
+                                       + NUMCHILD
+                                       + HHEAD + SHEAD + individual
+                                       + TOTFAMINC74
+                                       + MHYRSSCHL + FHYRSSCHL
+                                       + MH_SCH + FH_SCH
+                                       + MHLABPART + FHLABPART
+                                       + FHTOTERN74 + FHTOTERN73
+                                       + MHTOTERN74 + MHTOTERN73
+                                       + MHCHDCARE + FHCHDCARE
+                                       + MHJOBSAT + FHJOBSAT
+                                    + TOTNHINC73
+                                       + TOTNHINC74
+                                       , data = nonpart,
+                                              max.ylev = 10 )
+export2latex(createTable(compare))  
+
+
