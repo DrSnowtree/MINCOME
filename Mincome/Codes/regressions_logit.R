98681af64843dc0d8359969a61d2b05c74a1beda
Tuna Dökmeci <tuna.dokmeci@sciencespo.fr>
2020-01-09 10:47:21 +0100

    data changed

--- Mincome/Codes/regressions_logit.R
+++ Mincome/Codes/regressions_logit.R
@@ -56,3 +56,21 @@ reg6 <- glm(formula = if_birth ~ plan_1 + plan_2 + plan_3 + plan_4 +
 
 stargazer(reg5, reg6)
 
+#robustness 
+
+
+reg7 <- glm(formula = if_birth ~ treated, family = binomial(link = "logit"), data = basepay)
+
+summary(reg7)
+
++ DH + age1519 + age2024 + age2429 + 
+  age3034 + age3539 + age4044 + age4550 + FSI + NumChild  + incbracket  +
+  chout + if_birth9
+
+reg2 <- glm(formula = if_birth ~ plan_1 + plan_2 + plan_3 + plan_4 + 
+              plan_5 + plan_7 + plan_8 + DH + age1519 + age2024 + age2429 + 
+              age3034 + age3539 + age4044 + age4550  +
+              FSI + NumChild  + incbracket +
+              chout + if_birth9, family = binomial(link = "logit"), data = basepay)
+
+
